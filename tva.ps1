8abea870cbf9bafcdeec8f05d908bec35f9f50ee
Andre Kradolfer <narfdre@gmail.com>
2023-11-27 14:49:32 -0700

    adding setup scripts

---
+++ tva.ps1
@@ -0,0 +1,29 @@
+param(  
+  [Parameter(Mandatory)]  
+  [string]$new_api_key  
+)  
+$api_url = "https://wakapi.getperspect.xyz/api/v1"
+$URL = "https://perspect.xyz/plugins"
+$file_path = "$env:USERPROFILE\.wakatime\wakatime-internal.cfg"
+
+if (-not (Test-Path $file_path)) {
+    New-Item -ItemType File -Path $file_path | Out-Null
+    Write-Host "File created: $file_path"
+}
+
+# Check if any line contains "api_key ="
+if (Get-Content $file_path | Select-String "api_key =") {
+    # Update the line with the new API key
+    (Get-Content $file_path) | ForEach-Object {
+        $_ -replace "api_key = .*", "api_key = $new_api_key" `
+           -replace "api_url = .*", "api_url = $api_url"
+    } | Set-Content $file_path
+    Write-Host "API key updated in $file_path"
+} else {
+    # Add a new line with the API key
+    Add-Content $file_path "api_key = $new_api_key"
+    Add-Content $file_path "api_url = $api_url"
+    Write-Host "API key added to $file_path"
+}
+
+Start-Process "explorer" -ArgumentList $URL
---
+++ tva.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+api_url="https://wakapi.getperspect.xyz/api/v1"
+URL="https://perspect.xyz/plugins"
+file_path="$HOME/.wakatime.cfg"
+new_api_key=$1
+
+if [ ! -e "$file_path" ]; then
+    touch "$file_path"
+    echo "File created: $file_path"
+fi
+
+# Check if any line contains "api_key ="
+if grep -q "api_key =" "$file_path"; then
+    # Update the line with the new API key
+    sed -i -e "s,api_key = .*,api_key = $new_api_key," "$file_path"
+    sed -i -e "s,api_url = .*,api_url = $api_url," "$file_path"
+    echo "API key updated in $file_path"
+else
+    # Add a new line with the API key
+    echo "api_key = $new_api_key" >> "$file_path"
+    echo "api_url = $api_url" >> "$file_path"
+    echo "API key added to $file_path"
+fi
+
+
+
+if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+        xdg-open $URL
+elif [[ "$OSTYPE" == "darwin"* ]]; then
+        open $URL
+elif [[ "$OSTYPE" == "cygwin" ]]; then
+        explorer $URL
+elif [[ "$OSTYPE" == "msys" ]]; then
+        explorer $URL
+elif [[ "$OSTYPE" == "freebsd"* ]]; then
+        xdg-open $URL
+else
+        echo "please visit $URL to download plugins."
+fi
